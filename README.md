029c7684da1be9441cb2de239e93440eeb4e3445
Andreas Vrachimi <avrachimi@hotmail.com>
2022-10-28 17:48:36 +0100

    Added Javascript for API calls, populating data

---
+++ README.md
@@ -0,0 +1,5 @@
+# Top Crypto Prices
+
+**Frontend: Javascript, HTML, CSS(Sass)**
+
+Showing top 10 cryptocurrency prices, 24-hour change, market cap and percentage change from All Time High
---
+++ index.js
@@ -0,0 +1,69 @@
+const mktCapFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0,
+  });
+
+  const priceFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+const API_BASE_URL = 'https://api.coingecko.com/api/v3/';
+
+const vs_currency = "usd",
+    order = "market_cap_desc",
+    per_page = 10;
+
+const options = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+fetch(`${API_BASE_URL}/coins/markets?vs_currency=${vs_currency}&order=${order}&per_page=${per_page}`, options)
+    .then((response) => response.json())
+    .then((data) => populateCryptoDetails(data));
+
+function populateCryptoDetails(data) {
+    let cryptoList = document.getElementById('crypto-list');
+
+    let counter = 1;
+    data.forEach(coin => {
+        let parentDiv = document.createElement('div');
+        parentDiv.className = 'list-item row';
+    
+        let number = document.createElement('span');
+        number.className = 'number';
+        number.innerHTML = counter;
+        let name = document.createElement('span');
+        name.className = 'name';
+        name.innerHTML = coin.name;
+        let change_24h = document.createElement('span');
+        let percent_change_24h = Math.round(coin.price_change_percentage_24h * 100) / 100;
+        change_24h.className = percent_change_24h >= 0 ? '24h-change green' : '24h-change red';
+        change_24h.innerHTML = `${percent_change_24h}%`;
+        let mktCap = document.createElement('span');
+        mktCap.className = 'mkt-cap';
+        mktCap.innerHTML = `${mktCapFormatter.format(coin.market_cap)}`;
+        let ATHPercentage = document.createElement('span');
+        let ath_percent = Math.round(coin.ath_change_percentage * 100) / 100;
+        ATHPercentage.className = ath_percent >= 0 ? 'ath-percentage green' : 'ath-percentage red';
+        ATHPercentage.innerHTML = `${ath_percent}%`; // TODO: change
+        let price = document.createElement('span');
+        price.className = 'price';
+        price.innerHTML = `${priceFormatter.format(coin.current_price)}`;
+    
+        parentDiv.appendChild(number);
+        parentDiv.appendChild(name);
+        parentDiv.appendChild(change_24h);
+        parentDiv.appendChild(mktCap);
+        parentDiv.appendChild(ATHPercentage);
+        parentDiv.appendChild(price);
+    
+        cryptoList.appendChild(parentDiv);
+
+        counter++;
+    });
+}
